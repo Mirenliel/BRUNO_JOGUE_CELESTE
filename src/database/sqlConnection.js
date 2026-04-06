be525244dce22c2c9fc7f7b1772acf2c7948cef3
João Victor Pimenta R <joaovictorp.r77@gmail.com>
2026-04-06 09:06:44 -0300

    template da estrutura MVC

---
+++ src/database/sqlConnection.js
@@ -0,0 +1,19 @@
+import { Sequelize } from "sequelize";
+
+const sequelize = new Sequelize('projetointegrador', 'root', 'senai', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    storage: './database.sqlite'
+})
+
+async function connect() {
+    try {
+        await sequelize.authenticate();
+        console.log('conexão com o banco de dados estabelecida!');
+    } catch (error) {
+        console.error('erro ao conectar:', error);
+    }
+}
+
+export { sequelize, connect };
